39c82cf312ac538e36fc70918061197d286ad377
ShahulAndro <shahul.446@gmail.com>
2022-08-21 12:10:53 +0800

    Initial Project Setup

---
+++ PopoverSwiftLibrary/PopoverSwiftLibrary.h
@@ -0,0 +1,18 @@
+//
+//  PopoverSwiftLibrary.h
+//  PopoverSwiftLibrary
+//
+//  Created by Shahul Hameed Shaik on 21/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PopoverSwiftLibrary.
+FOUNDATION_EXPORT double PopoverSwiftLibraryVersionNumber;
+
+//! Project version string for PopoverSwiftLibrary.
+FOUNDATION_EXPORT const unsigned char PopoverSwiftLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopoverSwiftLibrary/PublicHeader.h>
+
+
---
+++ PopoverSwiftLibraryExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopoverSwiftLibraryExamples
+//
+//  Created by Shahul Hameed Shaik on 21/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
